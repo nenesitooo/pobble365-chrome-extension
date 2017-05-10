2b9ed3a562eaa6b37aeeb1a14a20777baad7f033
Anas Alaoui <anasse.alaoui@gmail.com>
2017-05-10 22:04:36 +0100

    🚀

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict'
+
+  const base = 'http://www.pobble365.com'
+
+  fetch(`${base}/api/v1`)
+    .then(response => response.json())
+    .then(data => getTodayPicture(data.resource_id))
+
+  function getTodayPicture (resourceId) {
+    fetch(`${base}/api/v1/resources/${resourceId}`)
+      .then(response => response.json())
+      .then(resource => render(resource))
+  }
+
+  function render (resource) {
+    document.body.style.backgroundImage = `url(${resource.picture_of_the_day.url}`
+    document.querySelector('h1').textContent = resource.title
+  }
+})()
